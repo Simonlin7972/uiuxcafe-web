1bfc6e9a25d9ea0ea14251273fe7b9579236674d
Simonlin7972 <a97210230@gmail.com>
2016-11-14 13:51:12 +0800

    First
    First

---
+++ js/all.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+    // job-detail切換選單
+    $('.tab-company').click(function(event) {
+        event.preventDefault();
+        $('.job-info').addClass('hide').removeClass('show');
+        $('.company-info').addClass('show').removeClass('hide');
+        $('.tab-company').children().addClass('tab-company-control');
+        $('.tab-job').children().removeClass('tab-company-control');
+    });
+
+    // job-detail切換選單
+    $('.tab-job').click(function(event) {
+        event.preventDefault();
+        $('.company-info').addClass('hide').removeClass('show');
+        $('.job-info').addClass('show').removeClass('hide');
+        $('.tab-company').children().removeClass('tab-company-control');
+        $('.tab-job').children().addClass('tab-company-control');
+    });
+
+    // nav-menu滿版選單開關
+    $('.menu-cancel').click(function(event) {
+        $('.menu-full').fadeToggle(300);
+
+    });
+    $('.menu-btn').click(function(event) {
+        $('.menu-full').fadeToggle(300);
+    });
+
+    // footer的註冊按鈕
+    $('.btn-signup').click(function(event) {
+        event.preventDefault();
+        $('.signup-full').fadeToggle(300);
+    });
+
+
+    // 刪除職缺提醒視窗
+    $('.del-job').click(function(event) {
+        swal({
+                title: "確定要刪除職缺？",
+                text: "刪除後將無法復原",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#EB223B",
+                confirmButtonText: "確認刪除",
+                cancelButtonText: "取消",
+                closeOnConfirm: false,
+                closeOnCancel: true,
+            },
+            function(isConfirm) {
+                if (isConfirm) {
+                    swal("刪除完成", "", "success");
+                }
+            });
+    });
+    $('.apply-job').click(function() {
+        $('.apply-job-content').fadeToggle(300);
+    });
+
+});
